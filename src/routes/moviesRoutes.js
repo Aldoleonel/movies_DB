3993ffcb5043d0f010708def06195ba6410f0447
Aldoleonel <aldoleonelchalup@gmail.com>
2023-09-27 06:28:06 -0300

    tp-terminado

---
+++ src/routes/moviesRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController');
+const createvalidator = require('../validations/createvalidator');
+const updatevalidator = require('../validations/updatevalidator');
+
+
+router.get('/movies', moviesController.list);
+router.get('/movies/new', moviesController.new);
+router.get('/movies/recommended', moviesController.recomended);
+router.get('/movies/detail/:id', moviesController.detail);
+
+
+//Rutas exigidas para la creación del CRUD
+router.get('/movies/add', moviesController.add);
+router.post('/movies/create',createvalidator, moviesController.create);
+router.get('/movies/edit/:id', moviesController.edit);
+router.put('/movies/update/:id',updatevalidator, moviesController.update);
+router.get('/movies/destroy/:id', moviesController.delete);
+router.delete('/movies/delete/:id', moviesController.destroy);
+
+
+module.exports = router;
